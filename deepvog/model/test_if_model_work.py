e68668237b1484dd7a8624f0e43b5ddfae14d8fe
Ubuntu <ubuntu@ip-172-31-16-205.ec2.internal>
2023-12-14 21:20:45 +0000

    Update setup.py

--- deepvog/model/test_if_model_work.py
+++ deepvog/model/test_if_model_work.py
@@ -1,14 +1,18 @@
 import numpy as np
 import skimage.io as ski
 from DeepVOG_model import load_DeepVOG
-
+import cv2
 
 def test_if_model_work():
     model = load_DeepVOG()
     img = np.zeros((1, 240, 320, 3))
-    img[:, :, :, :] = (ski.imread("test_image.png") / 255).reshape(1, 240, 320, 1)
+    # img[:, :, :, :] = (ski.imread("test_image.png") / 255).reshape(1, 240, 320, 1)
+    img[:, :, :, :] = (cv2.cvtColor(cv2.imread("test_image.png"), cv2.COLOR_BGR2GRAY).astype(np.float32) / 255).reshape(1, 240, 320, 1)
+    # for onnx
+    # img = img.transpose(0, 3, 1, 2) # (B, C, H, W)
     prediction = model.predict(img)
-    ski.imsave("test_prediction.png", prediction[0, :, :, 1])
+    # ski.imsave("test_prediction.png", prediction[0, :, :, 1])
+    cv2.imwrite("test_prediction.png", prediction[0, :, :, 1] * 255)
 
 
 if __name__ == "__main__":
